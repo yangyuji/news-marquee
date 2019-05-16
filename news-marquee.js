4e92154835a98717b1b625ab63aae948f1a77ef4
yangyuji <394712273@qq.com>
2019-05-16 10:26:42 +0800

    update

--- news-marquee.js
+++ news-marquee.js
@@ -3,7 +3,7 @@
 * license: "MIT",
 * name: "newsMarquee.js",
 * github: "https://github.com/yangyuji/news-marquee",
-* version: "1.0.3"
+* version: "1.0.4"
 */
 
 (function (root, factory) {
@@ -22,8 +22,11 @@
         this.oMarquee = typeof el == 'string' ? document.querySelector(el) : el;
         if (!this.oMarquee) return;
 
+        this.copyNode = _self.oMarquee.children[0].cloneNode(true);
         this.iLineHeight = this.oMarquee.children[0].offsetHeight;
         this.iLineCount = this.oMarquee.children.length;
+        this.timer1 = null
+        this.timer2 = null
 
         this.options = {
             successive: false,  //是否连续
@@ -43,22 +46,31 @@
                 _self.oMarquee.scrollTop = 0;
             }
             if (_self.options.successive) {
-                setTimeout(run, _self.options.speed);
+                _self.timer1 && clearTimeout(_self.timer1);
+                _self.timer1 = setTimeout(run, _self.options.speed);
             } else {
                 if(_self.oMarquee.scrollTop % _self.iLineHeight === 0) {
-                    setTimeout(run, _self.options.pause);
+                    _self.timer2 && clearTimeout(_self.timer2);
+                    _self.timer2 = setTimeout(run, _self.options.pause);
                 } else {
-                    setTimeout(run, _self.options.speed);
+                    _self.timer1 && clearTimeout(_self.timer1);
+                    _self.timer1 = setTimeout(run, _self.options.speed);
                 }
             }
         }
 
         function appendChild() {
-            var node = _self.oMarquee.children[0].cloneNode(true);
-            _self.oMarquee.appendChild(node);
+            _self.oMarquee.appendChild(_self.copyNode);
         }
 
         run();
+
+        // 停止当前滚动，释放定时器和添加的元素
+        this.destroy = function () {
+            _self.timer1 && clearTimeout(_self.timer1);
+            _self.timer2 && clearTimeout(_self.timer2);
+            _self.copyNode.parentNode.removeChild(_self.copyNode)
+        }
     }
 
     return Marquee;
